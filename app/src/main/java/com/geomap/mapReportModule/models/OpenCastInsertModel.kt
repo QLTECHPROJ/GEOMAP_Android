fbc19c579a9f0a36cb5937fb3bc7a66ab243e182
Mansi <mansivaghela804@gmail.com>
2022-11-09 13:53:36 +0100

    Model file.

---
+++ app/src/main/java/com/geomap/mapReportModule/models/OpenCastInsertModel.kt
@@ -0,0 +1,35 @@
+package com.geomap.mapReportModule.models
+
+import android.os.Parcel
+import android.os.Parcelable
+
+class OpenCastInsertModel{
+    var ocDate : String? = ""
+    var sheetNo : String? = ""
+    var minesSiteName : String? = ""
+    var pitName : String? = ""
+    var pitLocation : String? = ""
+    var shiftInchargeName : String? = ""
+    var geologistName : String? = ""
+    var faceLocation : String? = ""
+    var faceLengthM : String? = ""
+    var faceAreaM2 : String? = ""
+    var faceRockTypes : String? = ""
+    var benchRL : String? = ""
+    var benchHeightWidth : String? = ""
+    var benchAngle : String? = ""
+    var dipDirectionAngle : String? = ""
+    var thicknessOfOre : String? = ""
+    var thinessOfOverburden : String? = ""
+    var thicknessOfInterburden : String? = ""
+    var observedGradeOfOre : String? = ""
+    var actualGradeOfOre : String? = ""
+    var sampleCollected : String? = ""
+    var weathering : String? = ""
+    var rockStregth : String? = ""
+    var waterCondition : String? = ""
+    var typeOfGeologist : String? = ""
+    var typeOfFaults : String? = ""
+    var shift : String? = ""
+    var notes : String? = ""
+}
