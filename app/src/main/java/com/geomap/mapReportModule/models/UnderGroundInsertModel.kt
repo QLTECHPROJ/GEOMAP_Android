3e622f6e8b9de54399475c22023c0249e1057ad6
Mansi <mansivaghela804@gmail.com>
2022-11-15 08:36:49 +0100

    Under ground API Changes.

---
+++ app/src/main/java/com/geomap/mapReportModule/models/UnderGroundInsertModel.kt
@@ -0,0 +1,22 @@
+package com.geomap.mapReportModule.models
+
+data class UnderGroundInsertModel(
+    var minesSiteName : String? = null,
+    var name : String? = null,
+    var shift : String? = null,
+    var mappedBy : String? = null,
+    var scale : String? = null,
+    var location : String? = null,
+    var venieLoad : String? = null,
+    var xCordinate : String? = null,
+    var yCordinate : String? = null,
+    var zCordinate : String? = null,
+    var comment : String? = null,
+    var mapSerialNo : String? = null,
+    var ugDate : String? = null,
+    var leftImage : String? = null,
+    var roofImage : String? = null,
+    var rightImage : String? = null,
+    var faceImage : String? = null,
+    var attribute : String? = null
+)
